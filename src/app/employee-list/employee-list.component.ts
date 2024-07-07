ea442fdd4f3b741339f3e7a4c02e1c91a3decec2
Dipyaman De <dedipu17@gmail.com>
2024-07-07 12:01:32 +0530

    Frontend commit

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee} from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+employees?: Employee[];
+
+constructor(private employeeService:EmployeeService,private router:Router){}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  
+//   this.employees=[{
+//     "id":1,
+//     "firstName":"Ramesh",
+//     "lastName":"R",
+//     "emailId":"ramesh@gmail.com"
+//   },
+// {
+//  "id":2,
+//     "firstName":"Sasi",
+//     "lastName":"S",
+//     "emailId":"sasi@gmail.com" 
+// }
+// ]   
+}
+employeedetails(id:number)
+{
+  this.router.navigate(['employee-details',id])
+}
+updateEmployee(id:number)
+{
+  this.router.navigate(['update-employee',id])
+}
+
+  
+
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe( data => {
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+}
